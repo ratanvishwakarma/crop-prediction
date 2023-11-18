2ce41c4c00702a62d6aa246a2b37a03cb2886841
beunique123 <70933172+beunique123@users.noreply.github.com>
2023-11-18 23:20:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle 
 import pandas as pd
 
+model = pickle.load(open('model.sav','rb'))
 
 st.markdown("<h1 style='text-align: center;'>AMAA</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: grey;'>CROP PREDICTION SYSTEM🌱</h3>", unsafe_allow_html=True)
@@ -16,7 +17,7 @@ ph = float(st.number_input('Ph', 0,100))
 btn = st.button("Crop Prediction")
 
 if btn:
-    pred = np.array([N,P,K,temperature,humidity,ph]).reshape(1,-1)
+    pred = model(np.array([N,P,K,temperature,humidity,ph]).reshape(1,-1))
     print(pred)
     print(np.array(pred))
     pred = np.array([*pred])
