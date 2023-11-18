ceb17360b11a0ee68a7d6e9177d7fcaed77d5ff4
beunique123 <70933172+beunique123@users.noreply.github.com>
2023-11-18 21:22:10 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import streamlit as st
+import pickle
+import sklearn
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+model = pickle.load(open('model.pkl','rb'))
+
+st.markdown("<h1 style='text-align: center;'>AMAA</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: grey;'>CROP PREDICTION SYSTEM🌱</h3>", unsafe_allow_html=True)
+
+
+N = float(st.number_input('Nitrogen', 0,100))
+P = float(st.number_input('Phosphorus', 0,100))
+K = float(st.number_input('Potassium', 0,100))
+temperature = float(st.number_input('Temperature', 0,100))
+humidity = float(st.number_input('Humidity', 0,100))
+ph = float(st.number_input('Ph', 0,100))
+
+btn = st.button("Crop Prediction")
+
+if btn:
+    pred = model.predict(np.array([N,P,K,temperature,humidity,ph]).reshape(1,-1))
+    print(pred)
+    print(np.array(pred))
+    pred = np.array([*pred])
+    print(pred)
+    st.subheader(*pred)
