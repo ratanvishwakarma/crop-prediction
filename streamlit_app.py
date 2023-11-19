a23ea51313745c1d51a3a2961ef87d6974ce162e
beunique123 <70933172+beunique123@users.noreply.github.com>
2023-11-20 01:14:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ ph = float(st.number_input('Ph', 0,100))
 btn = st.button("Crop Prediction")
 
 if btn:
-    pred = model(np.array([N,P,K,temperature,humidity,ph]).reshape(1,-1))
+    pred = model.predict(np.array([N,P,K,temperature,humidity,ph]).reshape(1,-1))
     print(pred)
     print(np.array(pred))
     pred = np.array([*pred])
