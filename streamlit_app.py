03ce605cf84bc279376388bd89901451dfd89568
beunique123 <70933172+beunique123@users.noreply.github.com>
2023-11-20 01:09:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pickle 
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 import sklearn
 
 model = pickle.load(open('model.pkl','rb'))
