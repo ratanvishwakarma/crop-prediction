78d2946c36f9a9b8658a1b9f158ac97db543e8b0
beunique123 <70933172+beunique123@users.noreply.github.com>
2024-04-08 22:56:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,14 +42,14 @@ def crop_details():
     st.subheader("Crop Details:")
     # Define your crop details here as a dictionary
     crop_details = {
-        'Crop': ['Apple', 'Banana', 'Blackgram', 'Chickpea', 'Coconut', 'Coffee', 'Cotton', 'Grapes', 'Jute', 'Kidneybeans', 'lentil', 'Maize', 'Mango', 'Mothbeans', 'Mungbeans', 'Muskmelon', 'Orange', 'Papaya', 'Pigeonpeas', 'Pomegranate', 'Rice', 'Watermelon'],  # Example crop names
-        'Nitrogen': ['0 - 40', '80 - 120', '20 - 60'],  # Example details
-        'Phosphorus': ['120 - 145', '70 - 95', '55 - 80'],  # Example details
-        'Potassium': ['195 - 205', '45 - 55', '15 - 25'],
-        'Temperature': ['21 - 24', '25 - 30', '25 - 35'],
-        'Humidity': ['90 - 95', '75 - 85', '60 - 70'],
-        'Ph': ['5 - 6.5', '5 - 6.5', '6 - 8'],
-    }
+        'Crop': ['Apple', 'Banana', 'Blackgram', 'Chickpea', 'Coconut', 'Coffee', 'Cotton', 'Grapes', 'Jute', 'Kidneybeans', 'lentil', 'Maize', 'Mango', 'Mothbeans', 'Mungbeans', 'Muskmelon', 'Orange', 'Papaya', 'Pigeonpeas', 'Pomegranate', 'Rice', 'Watermelon'],
+        'Nitrogen': ['0 - 40', '80 - 120', '20 - 60', '20 - 60', '0 - 40', '80 - 120', '100 - 140', '0 - 40', '60 - 100', '0 - 40', '0 - 40', '60 - 100', '0 - 40', '0 - 40', '0 - 40', '80 - 120', '0 - 40', '31 - 70', '0 - 40', '0 - 40', '0 - 40', '80 - 120'],  
+        'Phosphorus': ['120 - 145', '70 - 95', '55 - 80', '55 - 80', '5 - 30', '15 - 40', '35 - 60', '120 - 145', '35 - 60', '55 - 80', '38 - 80', '35 - 60', '15 - 40', '35 - 60', '35 - 60', '5 - 30', '5 - 30', '46 - 70', '55 - 80', '5 - 30', '35 - 60', '5 - 30'],  
+        'Potassium': ['195 - 205', '45 - 55', '15 - 25', '75 - 85', '25 - 35', '25 - 35', '15 - 25', '195 - 205', '35 - 45', '15 - 25', '15 - 34', '15 - 25', '25 - 35', '15 - 25', '15 - 25', '45 - 55', '5 - 15', '45 - 55', '15 - 25', '35 - 45', '15 - 25', '45 - 55'],
+        'Temperature': ['21 - 24', '25 - 30', '25 - 35', '17 - 21', '25 - 30', '23 - 28', '22 - 26', '8.8 - 42', '23 - 27', '15 - 25', '18 - 30', '18 - 26.5', '27 - 36', '24 - 32', '27 - 30', '27 - 30', '10 - 38', '23 - 44', '18 - 37', '18 - 25', '24 - 32', '24 - 27'],
+        'Humidity': ['90 - 95', '75 - 85', '60 - 70', '14 - 20', '90 - 100', '50 - 70', '75 - 85', '80 - 84', '70 - 90', '18 - 25', '40 - 80', '55 - 75', '45 - 55', '40 - 65', '80 - 90', '90 - 95', '90 - 95', '90 - 95', '30 - 70', '80 - 95', '40 - 65', '80 - 90'],
+        'Ph': ['5 - 6.5', '5 - 6.5', '6 - 8', '5 - 9', '5 - 6.5', '6 - 7.5', '5.8 - 8', '5.5 - 6.5', '6 - 7.5', '5.5 - 6', '5 - 6', '5.5 - 7', '4.5 - 7', '3 - 10', '6 - 7', '6 - 7', '6 - 8', '6.5 - 7', '4.5 - 7.5', '5.5 - 7.2', '3 - 10', '6 - 7'],
+    
     crop_details_df = pd.DataFrame(crop_details)
     # Display the crop details in a table
     st.table(crop_details_df)
