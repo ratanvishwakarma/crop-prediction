c003456b96f3f7f7d1ec5342914992030e6faa5b
beunique123 <70933172+beunique123@users.noreply.github.com>
2024-04-09 00:19:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,10 +66,10 @@ def crop_prediction():
     # Perform prediction when the button is clicked
     if btn:
         pred = model.predict(np.array([N,P,K,temperature,humidity,ph]).reshape(1,-1))
-        st.subheader(pred)
+        st.subheader(*pred)
         if selected_language == "Hindi":  # Convert to Hindi if selected language is Hindi
             pred = english_to_hindi(pred)
-            st.subheader(pred)
+            st.subheader(*pred)
 
 def crop_details():
     # Table section
